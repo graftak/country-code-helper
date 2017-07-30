e11411fb84456d465f0b9f405ec728402b80610a
graftak <bertvandergenugten@gmail.com>
2017-07-30 16:40:40 +0200

    Adjusted structure of files.

---
+++ src/CountryCodeHelper/Exception.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Country Code Helper.
+ *
+ * (c) Bert van der Genugten <bertvandergenugten@gmail.com>
+ *
+ * This source file is subject to the BSD-3-Clause License that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Graftak\CountryCodeHelper;
+
+class Exception extends \Exception
+{
+    // Country code helper related exception.
+}
